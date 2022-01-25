a3033e249ecd8648ef38633a701ce47eeb3076ec
JakubCzerwiec <jacob.czerwiec@gmail.com>
2022-01-25 08:40:20 +0100

    Small client corrections

--- src/about.js
+++ src/about.js
@@ -28,7 +28,7 @@
                 
                 const text = document.createElement('p');
                     text.className = 'aboutMe';
-                        text.innerHTML = 'My name is Michał Matysik. I have over 10-year experience in studying Paleozoic and Mesozoic tropical- and cool-water carbonates, evaporites, mixed carbonate-siliciclastic systems, and biosiliceous deposits, gained during my Ph.D. research, four postdoc projects, and current geoconsulting and academic works focused on the Barents Sea, continental Europe, and the Middle East. Not only these different projects gave me an opportunity to work with a broad range of depositional facies and environments (sabkhas, tidal flats, lagoons, high-energy shoals, warm- to cool-water reefs, open-platform storm-dominated settings), diagenetic processes (dissolution, karstification, silicification, various dolomitization models), reservoir geometries (simple layer-cake successions to highly heterogeneous reservoirs hosted in tectonically tilted strata), and economic accumulations (hydrocarbons, MVT mineralization). They also developed my proficiency in planning and using proper methods to solve specific sedimentological problems. Part of the results has been published in high-impact peer-reviewed journals (see publication list). By visiting areas of modern sedimentation along the Red Sea, Persian (Arabian) Gulf, Oman Gulf, and Jutland Peninsula, I have supplemented my theoretical knowledge about coastal carbonate-evaporitic and siliciclastic settings.</p>';
+                        text.innerHTML = 'My name is Michał Matysik. I have over 10-year experience in studying Paleozoic and Mesozoic tropical- and cool-water carbonates, evaporites, mixed carbonate-siliciclastic systems, and biosiliceous deposits, gained during my Ph.D. research, four postdoc projects, and current geoconsulting and academic works focused on the Barents Sea, continental Europe, and the Middle East. Not only these different projects gave me an opportunity to work with a broad range of depositional facies and environments (sabkhas, tidal flats, lagoons, high-energy shoals, warm- to cool-water reefs, open-platform storm-dominated settings), diagenetic processes (dissolution, karstification, silicification, various dolomitization models), reservoir geometries (simple layer-cake successions to highly heterogeneous reservoirs hosted in tectonically tilted strata), and economic accumulations (hydrocarbons, MVT mineralization). They also developed my proficiency in planning and using proper methods to solve specific sedimentological problems. Part of the results has been published in high-impact peer-reviewed journals (see <span class="pubBtn">publication list</span>). By visiting areas of modern sedimentation along the Red Sea, Persian (Arabian) Gulf, Oman Gulf, and Jutland Peninsula, I have supplemented my theoretical knowledge about coastal carbonate-evaporitic and siliciclastic settings.</p>';
                 
                 mainText.append(text);
 
@@ -55,8 +55,8 @@
         
 
         // Publication list button
-            const publicBtn = document.querySelector('.publicationList');
-            publicBtn.addEventListener('click', () => {
+            const pubBtn = document.querySelector('.pubBtn');
+            pubBtn.addEventListener('click', () => {
                 publicationsRender();
             });
 
