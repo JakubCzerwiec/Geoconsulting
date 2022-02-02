5aa63b24fe531998b4e2842198d7e3366139fe2c
JakubCzerwiec <jacob.czerwiec@gmail.com>
2022-02-02 06:12:11 +0100

    New pictures and some small style changes

--- src/descriptionsForPics.js
+++ src/descriptionsForPics.js
@@ -1,30 +1,56 @@
 let picDescription0 = 'Modern intertidal laminite (planar stromatolite) during ebb tide. It grades seaward into carbonate mud (bright area). Trucial Coast, Abu Dhabi, United Arab Emirates. ';
-let picDescription1 = 'Transition from warm-water carbonates and gypsum (lower half of the bright gray part), to temperate-water carbonates (upper half of the bright gray part), to spiculitic sediments (black part). The wall is ca. 50 m high. Permian, central Spitsbergen.';
-let picDescription2 = 'Plane-polarized (left) and SEM (right) views of tight spiculitic chert (lower part) and porous spiculite with excellent inter- and intraspicule porosity (upper part). Permian biosiliceous strata, central Spitsbergen.';
+
+let picDescription1 = 'Transition from warm-water carbonates and gypsum (lower half of gray part), to temperate-water carbonates (upper gray part), to spiculitic sediments (black part). Permian, central Spitsbergen.';
+
+let picDescription2 = 'PPL (left) and SEM (right) views of tight spiculitic chert (lower part) and porous spiculite with excellent inter- and intraspicule porosity (upper part). Permian biosiliceous strata, central Spitsbergen.';
+
 let picDescription3 = 'Planar-e dolomite with relics of bryozoan colony. Excellent intraparticle and intercrystalline porosity.';
-let picDescription4 = 'CL view of saddle dolomite (sd) with non-luminescent core and bright red luminescent margin, post-dated by poikilotopic calcite (pc) in fracture. Permian biosiliceous strata, central Spitsbergen.';
+
+let picDescription4 = 'CL view of fracture with saddle dolomite (sd) and later poikilotopic calcite (pc). Permian biosiliceous strata, central Spitsbergen.';
+
 let picDescription5 = 'Void filled with calcite (white crystals) and galena (dark). MVT mineralization in Middle Triassic carbonates of the Kraków-Silesia region, southern Poland.';
+
 let picDescription6 = 'Sponge mound (m) with the oldest scleractinian corals in the World. Middle Triassic carbonates, southern Poland.';
-let picDescription7 = 'Bryozoan floatstone (lower half) being completely obliterated by late dolomitization and dissolution (upper half). ';
-let picDescription8 = 'Sabkha with desiccation cracks (s) and evaporite pan (e), grading seaward into microbial laminites (m). Car tracks for scale. Trucial Coast, Abu Dhabi, United Arab Emirates.';
-let picDescription9 = 'Sub-recent reddish caliche. Cyprus.';
+
+let picDescription7 = 'Bryozoan floatstone (lower half) being completely obliterated by later dolomitization and dissolution (upper half).';
+
+let picDescription8 = 'Sabkha with desiccation cracks (s) and evaporite pan (e), grading seaward into microbial laminites (m). Trucial Coast, Abu Dhabi, United Arab Emirates.';
+
+let picDescription9 = 'Cryptalgal laminite with fluidization conduits (white arrows) and synsedimentary fault (yellow arrow), recording syndepositional tectonic activity. Middle Triassic carbonates, southern Poland.';
+
 let picDescription10 = 'Karst-collapse breccias (arrows) in Upper Paleozoic carbonates. Central Spitsbergen.';
-let picDescription11 = 'Microbial mounds. Car tracks for scale. Early Devonian carbonates, northwestern Saudi Arabia.';
-let picDescription12 = 'Landward termination of the tidal channel, cutting carbonate mud during ebb tide. Saudi Arabia.';
+
+let picDescription11 = 'Microbial mounds. Early Devonian carbonates, northwestern Saudi Arabia.';
+
+let picDescription12 = 'CL view of recrystallized coarse, planar-s dolomite with blotchy luminescence. Carboniferous carbonates, Barents Sea.';
+
 let picDescription13 = 'Calcite-replaced nodules after anhydrite/gypsum. Middle Triassic carbonates, northern Poland.';
-let picDescription14 = 'Shoreface sandstone penetrated by Zoophycos (black structures) and topped with flooding surface (not shown). The surface is overlain by deep spiculite. Permian biosiliceous strata, central Spitsbergen.';
+
+let picDescription14 = 'Shoreface sandstone penetrated by Zoophycos (arrows) and directly overlain by deep spiculite (not shown). Permian biosiliceous strata, central Spitsbergen.';
+
 let picDescription15 = 'Sub-recent karst. Triassic carbonates, Dachstein platform, Austria.';
-let picDescription16 = 'Vertical transition from undolomitized micritic firmgrounds with Balanoglossites burrows (lower half), to epigenetically dolomitized firmgrounds (upper half). Middle Triassic carbonates, southern Poland.';
-let picDescription17 = 'Black spiculites cut by fractures with megaquartz (arrow) and later wide vein with saddle dolomite (orange cement; sd) and poikilotopic calcite (white cement; pc). Permian biosiliceous strata, central Spitsbergen.';
+
+let picDescription16 = 'Vertical transition from undolomitized (lower half) to epigenetically dolomitized (upper half) micritic firmgrounds with Balanoglossites burrows. Middle Triassic carbonates, southern Poland.';
+
+let picDescription17 = 'Black spiculites cut by fracture with megaquartz (arrow) and later vein with saddle dolomite (sd) and poikilotopic calcite (pc). Permian biosiliceous strata, central Spitsbergen.';
+
 let picDescription18 = 'Solution voids (hydrothermal karst) lined with several generations of white and orange saddle dolomite (sd) and later calcite (cc). Host rock (h) is Middle Devonian dolostone, southern Poland.';
+
 let picDescription19 = 'Layered lagoonal dolomicrite with superimposed dissolution vugs aligned with the layering. Middle Triassic carbonates, southern Poland.';
+
 let picDescription20 = 'Lofer cycles manifested as horizontal bedding in Upper Triassic carbonates. The Dolomites, northern Italy.';
+
 let picDescription21 = 'Middle Triassic peritidal dolostones (m) and cliff breccia (b), adjacent to the Devonian horst (h) that was standing in the middle of the Middle Triassic sea. Southern Poland.';
-let picDescription22 = 'Ore-bearing dolomite (epigenetically dolomitized micritic firmground) pierced by fractures with saddle dolomite (reddish cement) and Zn-Pb mineralization (dark cement). Middle Triassic carbonates, southern Poland.';
-let picDescription23 = 'Cryptalgal laminite with fluidization conduits (white arrows) and synsedimentary fault (yellow arrow). Both features indicate syndepositional tectonic activity. Middle Triassic carbonates, southern Poland.';
-let picDescription24 = 'Seismically induced jigsaw-puzzle (autoclastic) breccia in lagoonal dolomudstone. Middle Triassic carbonates, southern Poland.';
-let picDescription25 = 'K/T boundary (arrow) at Stevns Klint, Denmark.';
-let picDescription26 = 'CL view of partly recrystallized coarse, planar-s dolomite with characteristic blotchy luminescence.';
+
+let picDescription22 = 'Epigenetically dolomitized micritic firmground pierced by fractures with saddle dolomite (reddish cement) and galena (dark cement). Middle Triassic carbonates, southern Poland.';
+
+let picDescription23 = 'Seismically induced jigsaw-puzzle (autoclastic) breccia in lagoonal dolomudstone. Middle Triassic carbonates, southern Poland.';
+
+let picDescription24 = 'K/T boundary (arrow) at Stevns Klint, Denmark.';
+
+let picDescription25 = 'Landward termination of the tidal channel, cutting carbonate mud during ebb tide. Saudi Arabia.';
+
+let picDescription26 = 'Sub-recent reddish caliche. Cyprus.';
 
 let picDescriptions = [picDescription0, picDescription1, picDescription2, picDescription3, picDescription4, picDescription5, picDescription6, picDescription7, picDescription8, picDescription9, picDescription10, picDescription11, picDescription12, picDescription13, picDescription14, picDescription15, picDescription16, picDescription17, picDescription18, picDescription19, picDescription20, picDescription21, picDescription22, picDescription23, picDescription24, picDescription25, picDescription26];
 
